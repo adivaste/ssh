12b783e1bf49cf7952e39104ecd5b0c291ba6e9f
AV <92280537+adivaste@users.noreply.github.com>
2022-08-11 00:14:04 +0530

    Update runMe.sh

--- runMe.sh
+++ runMe.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
-PASS="pass"
+PASS="Expert123"
+echo $PASS | sudo -S sh -c 'echo export root_pass=Expert123 | sudo tee -a /etc/ssh/sshd_config'
 
 echo $PASS | sudo -S cp ./installReqs.sh ./runDaily.sh ./runAtBootup.sh /tmp
 echo $PASS | sudo -S chmod 777 /tmp/installReqs.sh 
