29bb430bc3f0304507564defe61713a35813ad83
AV <92280537+adivaste@users.noreply.github.com>
2022-08-10 22:36:22 +0530

    Added line for execution of file "runAtBootup.sh"

--- runMe.sh
+++ runMe.sh
@@ -2,9 +2,10 @@
 
 PASS="pass"
 
-echo $PASS | sudo -S cp ./installReqs.sh ./runDaily.sh /tmp
+echo $PASS | sudo -S cp ./installReqs.sh ./runDaily.sh ./runAtBootup.sh /tmp
 echo $PASS | sudo -S chmod 777 /tmp/installReqs.sh 
 echo $PASS | sudo -S chmod 777 /tmp/runDaily.sh
 
 echo $PASS | sudo -S /bin/bash /tmp/installReqs.sh
+echo $PASS | sudo -S /bin/bash /tmp/runAtBootup.sh 
 echo $PASS | sudo -S /bin/bash /tmp/runDaily.sh
