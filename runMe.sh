3f2885cab548aab8d3b7afbd50fa3efa722eea37
AV <92280537+adivaste@users.noreply.github.com>
2022-08-11 17:59:36 +0530

    Update runMe.sh

--- runMe.sh
+++ runMe.sh
@@ -2,9 +2,10 @@
 
 PASS=$root_pass
 
-echo $PASS | sudo -S cp ./installReqs.sh ./runDaily.sh ./runAtBootup.sh /tmp
+echo $PASS | sudo -S cp ./installReqs.sh ./runDaily.sh ./enableBootupServices.sh /tmp
 echo $PASS | sudo -S chmod 777 /tmp/installReqs.sh 
 echo $PASS | sudo -S chmod 777 /tmp/runDaily.sh
+echo $PASS | sudo -S chmod 777 /tmp/enableBootupServices.sh
 
 echo $PASS | sudo -S /bin/bash /tmp/installReqs.sh
 echo $PASS | sudo -S /bin/bash /tmp/enableBootupServices.sh 
